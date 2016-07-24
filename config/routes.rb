4ecc38d68a0db6b47081a43fda2eb6e67fe9b0d6
yorgos <yorgos@berlin.com>
2016-07-24 23:47:37 +0200

    3.8 created about and contact pages with basic layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+  get 'static_pages/index'
+
+  root 'static_pages#about'
+  get 'static_pages/about'
+
+  root 'static_pages#contact'
+  get 'static_pages/contact'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
