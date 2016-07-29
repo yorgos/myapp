f2e9aa1b484e1d4eec7b6de7d020c5d81b4ef50e
yorgos <yorgos@berlin.com>
2016-07-29 14:44:35 +0200

    4.1 scaffolding was run created products editted html

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  resources :products
+
   get 'static_pages/index'
-
-  root 'static_pages#about'
   get 'static_pages/about'
-
-  root 'static_pages#contact'
   get 'static_pages/contact'
 
+  root "static_pages#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
