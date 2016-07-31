fd44ad15756e057b342cdc354b19f599371c8009
yorgos <yorgos@berlin.com>
2016-07-31 19:34:47 +0200

    added colour field to products

---
+++ db/migrate/20160731171841_edited_product_description.rb
@@ -0,0 +1,4 @@
+class EditedProductDescription < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727134514) do
+ActiveRecord::Schema.define(version: 20160731172348) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160727134514) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
   end
 
 end
