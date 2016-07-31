44720cd5570c3985cf1d7f7cf437165548f850c3
yorgos <yorgos@berlin.com>
2016-08-01 01:50:02 +0200

    4.3 created new orders controller and vies

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   root "static_pages#index" #syntax = root "controller#view"
 
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
