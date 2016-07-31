cb620e06c4b264cf5cd1edba4e71db5018ca982b
yorgos <yorgos@berlin.com>
2016-08-01 00:05:37 +0200

    4.2 updated products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -72,6 +72,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :colour, :image_url)
+      params.require(:product).permit(:name, :description, :colour, :image_url, :price)
     end
 end
