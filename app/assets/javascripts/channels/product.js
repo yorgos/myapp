64835609c91ccb9376862b7a878daba7fc303c37
yorgos <yorgos@berlin.com>
2016-08-22 22:02:01 +0200

    6.14 advanced active cable and active job

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -11,5 +11,20 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
 
+    $('.product-reviews').prepend(data.comment);
+    refreshRating();
+  },
+
+  listen_to_comments: function() {
+    // ActionCable provides a function to call the methods of the ActionCable backend - including the one you just wrote. You call it with App.product.perform(). App is what you defined inside your application.js before. App.product represents the subscriber object for the product channel. As long as you're inside the object of the create() function, you can also simply refer to App.product as this. perform() expects at least one argument. The first argument is the name of the method you want to call. In this case, it's listen. Any additional arguments are going to be values for the arguments of the listen method. Since we defined a single argument for the listen method, we will add an object as second argument of the perform() function.
+    return this.perform('listen', {
+      product_id: $("[data-product-id]").data("product-id")
+    });
   }
+
+});
+
+// This will make sure that listen_to_comments() is called every time the user loads a new page.
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
