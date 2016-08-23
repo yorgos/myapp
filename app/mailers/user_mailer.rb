0e241f3ab12f809426b64a599cef6687561cc62f
yorgos <yorgos@berlin.com>
2016-08-23 21:41:32 +0200

    6.14.08 fixing and testing mailer for welcome mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
   end
 
   def welcome(user)
-    @appname = "Bike Shop"
+    @appname = "Flat Tire"
     mail( :to => user.email,
           :subject => "Welcome to #{@appname}!")
   end
