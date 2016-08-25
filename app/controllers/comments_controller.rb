3feb06861c560f0e63d84ddb8f556cd4407b9310
yorgos <yorgos@berlin.com>
2016-08-25 18:08:47 +0200

    6.14.15 fixing action cable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,8 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
 
+        ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, current_user)
+        
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
         # For the AJAX request
