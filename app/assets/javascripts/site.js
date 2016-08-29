0854e9876e76ff042d8547902bb8fc0035bd5ffc
yorgos <yorgos@berlin.com>
2016-08-29 17:28:18 +0200

    6.15.08 action cable

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,11 +3,20 @@
 // If there are any other event listeners running on $(document) itself (e.g. $(document).on("click", ...) that one should not be inside the $(document).on('turbolinks ... function. Instead, it should be next to it (either below or above).
 
 var refreshRating = function() {
+
+  // Fadein-Fadeout
+  $('.alert').delay(500).fadeIn('normal', function() {
+     $(this).delay(1500).fadeOut();
+  });
+
   // Raty Plugin
   $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]'});
 
-  $('.rated').raty({ path: '/assets', readOnly:true, score: function() {
-    return $(this).attr('data-score');
+  $('.rated').raty({
+    path: '/assets',
+    readOnly:true,
+    score: function() {
+      return $(this).attr('data-score');
     }
   });
 };
